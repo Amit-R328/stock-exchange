47a94be431208f4fe7c635ef66b924cf78d667da
Amit Rozen <amitrozen2015@gmail.com>
2025-07-13 20:21:03 +0300

    clean up

--- frontend/src/environments/environments.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://stock-exchange-backend-sfji.onrender.com/api/v1',
-  wsUrl: 'wss://stock-exchange-backend-sfji.onrender.com/ws'
-}
