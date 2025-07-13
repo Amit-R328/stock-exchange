436d2465e2a1d8637b8e2137b0336ae843eda831
Amit Rozen <amitrozen2015@gmail.com>
2025-07-13 20:18:47 +0300

    deployment

---
+++ frontend/src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://stock-exchange-backend-sfji.onrender.com/api/v1',
+  wsUrl: 'wss://stock-exchange-backend-sfji.onrender.com/ws'
+}
